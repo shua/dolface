784963492cd6cd24cd4fb15c4807523b751a274f
shuall <shualloret@gmail.com>
2014-07-27 23:43:02 -0400

    Seems to be working fine, only thing not identical is file offsets

--- fsticuff/fsticuff.c
+++ fsticuff/fsticuff.c
@@ -1,29 +1,20 @@
-
 //stat and dirent on windows?
 #include <sys/stat.h>
 #include <dirent.h>
 #include <string.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "fsticuff.h"
 
-struct FstEntry {
-    unsigned char type;
-    unsigned char str_off[3];
-    union {
-        unsigned int file_off;
-        unsigned int parent_off;
-    };
-    union {
-        unsigned int file_length;
-        unsigned int next_offset;
-        unsigned int num_entries;
-    };
-};
-#define FST_TFILE 0
-#define FST_TDIR  1
+// magic number, could be tied to /sys size, but I don't know
+// NOTE I don't understand how they calculate offsets, it seems to be
+//  0x20 aligned, like file sizes, but some files are pushed for no
+//  apparent reason.
+const int _initial_off = 0x45d640; 
 
+// swap functions
 int swap32(int in) {
     return 
         (in & 0xff) << 24 |
@@ -32,14 +23,175 @@ int swap32(int in) {
         (in & 0xff000000) >> 24;
 }
 
-int swap24c(unsigned char* in) {
-    return (in[0] << 16 | in[1] << 8 | in[2]);
+// fst enty struct and defines
+struct FstEntry {
+    int id;
+    union {
+        unsigned int file_off;
+        unsigned int parent_off;
+    };
+    union {
+        unsigned int file_length;
+        unsigned int next_off;
+        unsigned int num_entries;
+    };
+};
+
+int _ename(struct FstEntry e) { return swap32(e.id) & 0x00ffffff; }
+int _etype(struct FstEntry e) { return (swap32(e.id) & 0xff000000) >> 24; }
+struct FstEntry* _esetid(struct FstEntry* dest, int t, int s_idx) {
+    dest->id = swap32(((t & 0xff) << 24) | (s_idx & 0x00ffffff));
+    return dest;
+}
+#define FST_TFILE 0
+#define FST_TDIR  1
+
+// utility string array struct
+struct StrArray {
+    int size;
+    int cap;
+    char* m;
+};
+
+int insertStr(struct StrArray* dest, char* str) {
+    if(strlen(str) + dest->size >= dest->cap) {
+        char* tmp = malloc(2*dest->cap);
+        memcpy(tmp, dest->m, dest->size);
+        dest->cap *= 2;
+        free(dest->m);
+        dest->m = tmp;
+    }
+    int ret = dest->size;
+    strcpy(dest->m + dest->size, str);
+    dest->size += (strlen(str) + 1);
+    return ret;
 }
 
-int stat_dir(char* dir, FILE* fp);
-int stat_file();
+struct StrArray getStrArray(int icap) {
+    struct StrArray ret = { .size = 0, .cap = icap, .m = malloc(icap) };
+    return ret;
+}
 
+// recursive write functions
+struct IdxOff {
+    int idx;
+    unsigned int off;
+};
+
+int filter_dot(const struct dirent* d) { return (d->d_name[0] != '.'); }
+// for some reason, this is how the original fst is sorted
+//  first lowercase, then uppercase, then punctuation
+int _fststrcmp(const char* a, const char* b) {
+    int i=0,ga=0,gb=0;
+    while(a[i] == b[i] && a[i] != '\0' && b[i] != '\0') i++;
+    if(a[i] == '\0' || b[i] == '\0') return a[i] - b[i];
+
+    if(a[i] <= '9' && a[i] >= '0') ga=0;
+    else if(a[i] <= 'z' && a[i] >= 'a') ga=1;
+    else if(a[i] <= 'Z' && a[i] >='A') ga=2;
+    else ga=3;
+    if(b[i] <= '9' && b[i] >= '0') gb=0;
+    else if(b[i] <= 'z' && b[i] >='a') gb=1;
+    else if(b[i] <= 'Z' && b[i] >= 'A') gb=2;
+    else gb=3;
+
+    if(ga == gb)
+        return a[i] - b[i];
+    else
+        return ga - gb;
+}
+int fstsort(const struct dirent** a, const struct dirent** b) {
+    return _fststrcmp((*a)->d_name, (*b)->d_name);
+}
+
+struct IdxOff stat_dir(char* dirname, FILE* fp, struct IdxOff idxoff, unsigned int pidx, struct StrArray* strtab) {
+    struct dirent** namelist;
+    int namelistsz;
+    struct FstEntry entry = {0};
+    struct stat s;
+    int staterr;
+
+    namelistsz = scandir(dirname, &namelist, filter_dot, fstsort);
+    if(namelistsz < 0) {
+        perror("scandir");
+        idxoff.idx = -1;
+        return idxoff;
+    }
+    for (int n = 0; n < namelistsz; n++)  {
+        char path[strlen(dirname) + strlen(namelist[n]->d_name) + 2];
+        strcpy(path, dirname);
+        strcat(path, "/");
+        strcat(path, namelist[n]->d_name);
+
+        if((staterr = stat(path, &s)) != 0) {
+            printf("Error: could not stat %s: %d", path, staterr);
+            free(namelist[n]);
+            continue;
+        }
+
+        if(s.st_mode & S_IFDIR) {
+            struct IdxOff tmp = idxoff;
+            idxoff.idx++;
+            long curpos = ftell(fp);
+            int name = insertStr(strtab, namelist[n]->d_name);
+            
+            _esetid(&entry, FST_TDIR, name);
+            entry.parent_off = swap32(pidx);
+            fseek(fp, 12, SEEK_CUR);
+            
+            idxoff = stat_dir(path, fp, idxoff, tmp.idx, strtab);
+            if(idxoff.idx < 0) {
+                while(n < namelistsz)
+                    free(namelist[n++]);
+                free(namelist);
+                return idxoff;
+            }
+            
+            entry.next_off = swap32(idxoff.idx);
+            long nextpos = ftell(fp);
+            fseek(fp, curpos, SEEK_SET);
+            fwrite(&entry, 12, 1, fp);
+            fseek(fp, nextpos, SEEK_SET);
+
+        } else if(s.st_mode & S_IFREG) {
+            int size = (s.st_size / 0x20 + (((s.st_size % 0x20) == 0)?0:1)) * 0x20;
+            int name = insertStr(strtab, namelist[n]->d_name);
+            
+            _esetid(&entry, FST_TFILE, name);
+            entry.file_off = swap32(idxoff.off);
+            entry.file_length = swap32(size);
+
+            fwrite(&entry, 12, 1, fp);
+            
+            idxoff.off += size;
+            idxoff.idx++;
+        } else {
+            printf("Warning: %s is some kind of funky", path);
+        }
+
+        free(namelist[n]);
+    }
+    free(namelist);
+    return idxoff;
+}
+
+// main functions
 int generate_fst(char* gamedir, char* fileout) {
+    struct StrArray strtab = getStrArray(256);
+    FILE* fp = fopen(fileout, "w");
+    fseek(fp, 12, SEEK_SET);
+    struct IdxOff entries = { 1, _initial_off };
+    entries = stat_dir(gamedir, fp, entries, 0, &strtab);
+
+    fwrite(strtab.m, strtab.size, 1, fp);
+    free(strtab.m);
+
+    struct FstEntry root = { 0, 0, swap32(entries.idx) };
+    _esetid(&root, FST_TDIR, 0);
+    fseek(fp, 0, SEEK_SET);
+    fwrite(&root, 12, 1, fp);
+
+    fclose(fp);
     return 0;
 }
 
@@ -48,24 +200,24 @@ void print_fst(char* fst) {
     struct stat s;
     int staterr;
     if((staterr = stat(fst,&s)) != 0) {
-        printf("Error: failed to stat file %s : %d", fst, staterr);
+        printf("Error: failed to stat file %s : %d\n", fst, staterr);
         return;
     }
     if(!(s.st_mode & S_IFREG)) {
-        printf("Error: %s is not an fst file", fst);
+        printf("Error: %s is not an fst file\n", fst);
         return;
     }
     FILE* fp = fopen(fst, "r");
     if(!fp) {
-        printf("Error: Opening file %s", fst);
+        printf("Error: Opening file %s\n", fst);
         return;
     }
     struct FstEntry entry = {0};
     fread(&entry, 12, 1, fp);
-    if(entry.type != FST_TDIR 
-            || entry.str_off[0] & entry.str_off[1] & entry.str_off[2] != 0
+    if(_etype(entry) != FST_TDIR 
+            || _ename(entry) != 0
             || entry.file_off != 0) {
-        printf("Error: %s is not an fst file (incorrect root)", fst);
+        printf("Error: %s is not an fst file (incorrect root)\n", fst);
         return;
     }
 
@@ -76,23 +228,21 @@ void print_fst(char* fst) {
 
     fseek(fp, strtab_off, SEEK_SET);
     if(fread(strtab, strtab_sz, 1, fp) != 1) {
-        printf("Error: Reading string table: %ld %ld %d", strtab_off, strtab_sz, entries);
+        printf("Error: Reading string table: %ld %ld %d\n", strtab_off, strtab_sz, entries);
         free(strtab);
         fclose(fp);
         return;
     }
-    printf("strtab[0] : %s\n", strtab);
 
     rewind(fp);
     fread(&entry, 12, 1, fp);
-    printf("root %d; %x %x\n", swap32(entry.num_entries), swap24c(entry.str_off), swap32(entry.parent_off));
     for(int i = 1; i < entries; i++) {
         fread(&entry, 12, 1, fp);
         printf( "%s %08x %08x %s\n", 
-                (entry.type == FST_TDIR)?"d":"f",
+                (_etype(entry) == FST_TDIR)?"d":"f",
                 swap32(entry.file_off),
                 swap32(entry.file_length),
-                strtab + swap24c(entry.str_off));
+                strtab + _ename(entry));
     }
 
     free(strtab);
--- fsticuff/main.c
+++ fsticuff/main.c
@@ -1,23 +1,54 @@
 #include "fsticuff.h"
+#include <string.h>
 #include <stdio.h>
 
 void usage(int argc, char** argv) {
     printf("Usage: %s DIR|INPUT [OPTION] [OUTPUT]\n", argv[0]);
     printf("Creates gamecube fst file given directory DIR containing extracted iso\n");
     printf("Optional OUTPUT file name (Default is fst.bin[.bak])\n\n");
-    printf("  -f,   overwrite existing fst.bin (otherwise write fst.bin.bak\n");
+    printf("  -f    overwrite existing fst.bin (otherwise write fst.bin.bak\n");
+    printf("  -p    print INPUT info to stdout\n");
 }
 
+
+#define RF_PRINT 1<<0
+#define RF_FORCE 1<<1
+
 int main(int argc, char** argv) {
-    if(argc < 2) {
+    char* nargv[argc];
+    int nargc = 0;
+    int flags = 0;
+    for(int i=0; i < argc; i++) {
+        if(argv[i][0] == '-') {
+            for(int j=1; argv[i][j] != '\0'; j++) {
+                switch(argv[i][j]) {
+                    case 'p': flags |= RF_PRINT; break;
+                    case 'f': flags |= RF_FORCE; break;
+                    default: break;
+                }
+            }
+        } else {
+            nargv[nargc] = argv[i];
+            ++nargc;
+        }
+    }
+
+    if(nargc < 2) {
         usage(argc, argv);
         return 0;
-    } else if(argc < 3) {
-//        return generate_fst(argv[1], "fst.bin");
-        print_fst(argv[1]);
-        return 0;
-    } else {
-        return generate_fst(argv[1], "fst.bin");
     }
+
+    if(flags & RF_PRINT) {
+        print_fst(nargv[1]);
+        return 0;
+    }
+
+    while(nargv[1][strlen(nargv[1])-1] == '/')
+        nargv[1][strlen(nargv[1])-1] = '\0';
+    if(nargc > 2)
+        generate_fst(nargv[1], nargv[2]);
+    else
+        generate_fst(nargv[1], "fst.bin");
+    return 0;
 }
 
