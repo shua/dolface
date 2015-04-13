3288c1e00761c2a03de7326cdec61fcfb3821b9d
shuall <shualloret@gmail.com>
2015-04-13 19:50:47 -0400

    dol2elf print dol hdr

--- dol2elf/dol2elf.c
+++ dol2elf/dol2elf.c
@@ -4,63 +4,52 @@
 #include <string.h>
 #include <sys/param.h>
 
-#ifndef MAX
-//! Get the maximum of two values
-#define MAX(a, b)	(((a) > (b)) ? (a) : (b))
-#endif
-
-#ifndef MIN
-//! Get the minimum of two values
-#define MIN(a, b)	(((a) < (b)) ? (a) : (b))
-#endif
-
 #define EI_NIDENT       16
-
 typedef struct {
-	unsigned char	e_ident[EI_NIDENT];
-	uint16_t		e_type;
-	uint16_t		e_machine;
-	uint32_t		e_version;
-	uint32_t		e_entry;
-	uint32_t		e_phoff;
-	uint32_t		e_shoff;
-	uint32_t		e_flags;
-	uint16_t		e_ehsize;
-	uint16_t		e_phentsize;
-	uint16_t		e_phnum;
-	uint16_t		e_shentsize;
-	uint16_t		e_shnum;
-	uint16_t		e_shstrndx;
+    unsigned char       e_ident[EI_NIDENT];
+    uint16_t            e_type;
+    uint16_t            e_machine;
+    uint32_t            e_version;
+    uint32_t            e_entry;
+    uint32_t            e_phoff;
+    uint32_t            e_shoff;
+    uint32_t            e_flags;
+    uint16_t            e_ehsize;
+    uint16_t            e_phentsize;
+    uint16_t            e_phnum;
+    uint16_t            e_shentsize;
+    uint16_t            e_shnum;
+    uint16_t            e_shstrndx;
 } Elf32_Ehdr;
 
 
-#define EI_CLASS	4
-#define EI_DATA	    5
-#define EI_VERSION	6
-#define EI_PAD		7
+#define EI_CLASS        4
+#define EI_DATA         5
+#define EI_VERSION      6
+#define EI_PAD          7
 
-#define ELFCLASS32	1
-#define ELFDATA2MSB	2
-#define EV_CURRENT	1
+#define ELFCLASS32      1
+#define ELFDATA2MSB     2
+#define EV_CURRENT      1
 
-#define ET_EXEC		2
-#define EM_PPC		20
+#define ET_EXEC         2
+#define EM_PPC          20
 
 typedef struct {
-	uint32_t	p_type;
-	uint32_t	p_offset;
-	uint32_t	p_vaddr;
-	uint32_t	p_paddr;
-	uint32_t	p_filesz;
-	uint32_t	p_memsz;
-	uint32_t	p_flags;
-	uint32_t	p_align;
+    uint32_t    p_type;
+    uint32_t    p_offset;
+    uint32_t    p_vaddr;
+    uint32_t    p_paddr;
+    uint32_t    p_filesz;
+    uint32_t    p_memsz;
+    uint32_t    p_flags;
+    uint32_t    p_align;
 } Elf32_Phdr;
 
-#define PT_LOAD	1
-#define PF_R	4
-#define PF_W	2
-#define PF_X	1
+#define PT_LOAD 1
+#define PF_R    4
+#define PF_W    2
+#define PF_X    1
 
 typedef struct {
     uint32_t    s_name;
@@ -83,42 +72,17 @@ typedef struct {
 #define SHF_EXECINSTR 0x4
 #define SHF_LINK_ORDER 0x80
 
-int verbosity = 0;
-
-#if BYTE_ORDER == BIG_ENDIAN
-
-#define swap32(x) (x)
-#define swap16(x) (x)
-
-#else
-
-static inline uint32_t swap32(uint32_t v)
-{
-	return (v >> 24) |
-	       ((v >> 8)  & 0x0000FF00) |
-	       ((v << 8)  & 0x00FF0000) |
-	       (v << 24);
-}
-
-static inline uint16_t swap16(uint16_t v)
-{
-	return (v >> 8) | (v << 8);
-}
-
-#endif /* BIG_ENDIAN */
-
-typedef struct {
-
-	uint32_t text_off[7];
-	uint32_t data_off[11];
-	uint32_t text_addr[7];
-	uint32_t data_addr[11];
-	uint32_t text_size[7];
-	uint32_t data_size[11];
-	uint32_t bss_addr;
-	uint32_t bss_size;
-	uint32_t entry;
-	uint32_t pad[7];
+typedef struct { 
+    uint32_t text_off[7];
+    uint32_t data_off[11];
+    uint32_t text_addr[7];
+    uint32_t data_addr[11];
+    uint32_t text_size[7];
+    uint32_t data_size[11];
+    uint32_t bss_addr;
+    uint32_t bss_size;
+    uint32_t entry;
+    uint32_t pad[7];
 } DOL_hdr;
 
 #define HAVE_BSS 1
@@ -129,87 +93,100 @@ typedef struct {
 #define ELF_SEGMENT_ALIGNMENT 0x10000
 #define ELF_SECTION_ALIGNMENT 0x10
 
-#define ALIGN(x, a) (((x) + (a) - 1) & ~((a) - 1))
-
 typedef struct {
-	Elf32_Ehdr header;
-	Elf32_Phdr pheader[4];
+    Elf32_Ehdr header;
+    Elf32_Phdr pheader[4];
     Elf32_Shdr sheader[19];
     uint32_t dol_off[4];
     uint32_t shstrtabsz;
     char *shstrtab;
-	FILE *dol;
+    FILE *dol;
 } ELF_map;
 
-void usage(const char *p)
-{
-    int i=strlen(p);
-    while(i!=0 && p[i-1] != '/') { i--; }
-    const char* name = p+i;
-    fprintf(stderr, "Usage: %s [-h] [-v] [--] dol-file elf-file\n", name);
-    fprintf(stderr, " Convert a DOL file to an ELF file (by segments)\n");
-    fprintf(stderr, " Options:\n");
-    fprintf(stderr, "  -h    Show this help\n");
-    fprintf(stderr, "  -v    Be more verbose (twice for even more)\n");
+
+#ifndef MAX
+#define MAX(a, b)       (((a) > (b)) ? (a) : (b))
+#endif
+#ifndef MIN
+#define MIN(a, b)       (((a) < (b)) ? (a) : (b))
+#endif
+#define ALIGN(x, a) (((x) + (a) - 1) & ~((a) - 1))
+
+#if BYTE_ORDER == BIG_ENDIAN
+#define swap32(x) (x)
+#define swap16(x) (x)
+#else
+static inline uint32_t 
+swap32(uint32_t v) {
+    return (v >> 24) | ((v >> 8)  & 0x0000FF00) | ((v << 8)  & 0x00FF0000) | (v << 24);
 }
+
+static inline uint16_t 
+swap16(uint16_t v) {
+    return (v >> 8) | (v << 8);
+}
+#endif /* BIG_ENDIAN */
 
 #define die(x) { fprintf(stderr, x "\n"); exit(1); }
 #define perrordie(x) { perror(x); exit(1); }
+void ferrordie(FILE *, const char *);
 
-void ferrordie(FILE *f, const char *str)
-{
-	if(ferror(f)) {
-		fprintf(stderr, "Error while ");
-		perrordie(str);
-	} else if(feof(f)) {
-		fprintf(stderr, "EOF while %s\n", str);
-		exit(1);
-	} else {
-		fprintf(stderr, "Unknown error while %s\n", str);
-		exit(1);
-	}
+void read_dol_segments(ELF_map *, const char *);
+void map_elf(ELF_map *);
+void fcpy(FILE *, FILE *, uint32_t, uint32_t, uint32_t);
+void write_elf(ELF_map *, const char *);
+void print_dol_hdr(const char *);
+void usage(const char *p);
+
+int verbosity = 0;
+
+void 
+ferrordie(FILE *f, const char *str) {
+    if(ferror(f)) {
+        fprintf(stderr, "Error while ");
+        perrordie(str);
+    } else if(feof(f)) {
+        fprintf(stderr, "EOF while %s\n", str);
+        exit(1);
+    } else {
+        fprintf(stderr, "Unknown error while %s\n", str);
+        exit(1);
+    }
 }
 
 #define PHI_TEXT 1
 #define PHI_DATA 2
 #define PHI_BSS  3
 
-void read_dol_segments(ELF_map *map, const char *dol)
-{
+void 
+read_dol_segments(ELF_map *map, const char *dol) {
     int read, i;
     DOL_hdr hdr;
-    
-    if(verbosity >= 2)
-        fprintf(stderr, "Reading DOL file...\n");
+
+    if(verbosity >= 2) fprintf(stderr, "Reading DOL file...\n");
 
     map->dol = fopen(dol, "rb");
-    if(!map->dol)
-        perrordie("Could not open DOL file");
+    if(!map->dol) perrordie("Could not open DOL file");
 
     read = fread(&hdr, sizeof(hdr), 1, map->dol);
-    if(read != 1)
-        ferrordie(map->dol, "reading DOL header");
+    if(read != 1) ferrordie(map->dol, "reading DOL header");
 
     map->header.e_entry = hdr.entry;
 
-    if(verbosity >= 2)
-        fprintf(stderr, "Valid DOL header found\n");
+    if(verbosity >= 2) fprintf(stderr, "Valid DOL header found\n");
 
     int tsec = 0,dsec = 0;
     for(i=0;i<7;++i) {
         if(hdr.text_size[i] != 0)
             tsec = i+1;
     }
-    if(!tsec)
-        perrordie("No text sections");
+    if(!tsec) perrordie("No text sections");
     for(i=0;i<11;++i) {
         if(hdr.data_size[i] != 0)
             dsec = i+1;
     }
-    if(!dsec)
-        perrordie("No data sections");
-    if(verbosity >= 2)
-        fprintf(stderr, "%d text sections, and %d data sections in dol\n", tsec, dsec);
+    if(!dsec) perrordie("No data sections");
+    if(verbosity >= 2) fprintf(stderr, "%d text sections, and %d data sections in dol\n", tsec, dsec);
 
     map->pheader[PHI_TEXT].p_flags = swap32(PF_R | PF_X);
     map->dol_off[PHI_TEXT] = swap32(hdr.text_off[0]);
@@ -276,8 +253,8 @@ void read_dol_segments(ELF_map *map, const char *dol)
     map->sheader[i].s_size = hdr.bss_size;
 }
 
-void map_elf(ELF_map *map)
-{
+void 
+map_elf(ELF_map *map) {
     if(verbosity >= 2)
         fprintf(stderr, "Laying out ELF file...\n");
 
@@ -322,7 +299,7 @@ void map_elf(ELF_map *map)
         map->shstrtabsz += shstrtablen[i];
     }
     map->shstrtab = malloc(map->shstrtabsz * sizeof(char));
-    
+
     if(verbosity >= 2)
         fprintf(stderr, "length: %2d; size: %ld\n", map->shstrtabsz, map->shstrtabsz * sizeof(char));
 
@@ -355,8 +332,8 @@ void map_elf(ELF_map *map)
 
 #define BLOCK (1024*1024)
 
-void fcpy(FILE *dst, FILE *src, uint32_t dst_off, uint32_t src_off, uint32_t size)
-{
+void 
+fcpy(FILE *dst, FILE *src, uint32_t dst_off, uint32_t src_off, uint32_t size) {
     int left = size;
     int read;
     int written;
@@ -392,8 +369,8 @@ void fcpy(FILE *dst, FILE *src, uint32_t dst_off, uint32_t src_off, uint32_t siz
     free(blockbuf);
 }
 
-void write_elf(ELF_map *map, const char *elf)
-{
+void 
+write_elf(ELF_map *map, const char *elf) {
     FILE *elff;
     int written;
     int i;
@@ -403,7 +380,7 @@ void write_elf(ELF_map *map, const char *elf)
 
     elff = fopen(elf, "wb");
     if(!elff)
-        perrordie("Could not open DOL file");
+        perrordie("Could not open ELF file for writing");
 
     written = fwrite(&map->header, sizeof(Elf32_Ehdr), 1, elff);
     if(written != 1)
@@ -419,7 +396,7 @@ void write_elf(ELF_map *map, const char *elf)
         if(verbosity >= 2)
             fprintf(stderr, "Writing segment %d...\n", i);
         fcpy(elff, map->dol, swap32(map->pheader[i].p_offset), map->dol_off[i],
-             swap32(map->pheader[i].p_filesz));
+                swap32(map->pheader[i].p_filesz));
     }
 
     if(swap32(map->sheader[1].s_size) != 0) {
@@ -449,9 +426,43 @@ void write_elf(ELF_map *map, const char *elf)
     fclose(elff);
 }
 
-int main(int argc, char** argv)
-{
+void
+print_dol_hdr(const char* dol_file) {
+    int i;
+    DOL_hdr hdr;
+    FILE *fp;
+
+    if(!(fp=fopen(dol_file, "r"))) perrordie("Couldn't open DOL file for reading\n");
+    if(fread(&hdr, sizeof(DOL_hdr), 1, fp) != 1) perrordie("Couldn't read header from DOL file");
+    fclose(fp);
+
+    printf("name  | %8s %8s %8s\n", "off", "addr", "size");
+    for(i=0; i<7; ++i) if(hdr.text_off[i]) printf("text%x | %8x %8x %8x\n", i, swap32(hdr.text_off[i]), swap32(hdr.text_addr[i]), swap32(hdr.text_size[i]));
+    for(i=0; i<11; ++i) if(hdr.data_off[i]) printf("data%x | %8x %8x %8x\n", i, swap32(hdr.data_off[i]), swap32(hdr.data_addr[i]), swap32(hdr.data_size[i]));
+    printf("bss   |          %8x %8x\n", swap32(hdr.bss_addr), swap32(hdr.bss_size));
+    printf("entry |          %8x\n", swap32(hdr.entry));
+}
+
+void 
+usage(const char *p) {
+    int i=strlen(p);
+    while(i!=0 && p[i-1] != '/') { i--; }
+    const char* name = p+i;
+    fprintf(stderr, "Usage: %s [-h] [-v] [--] dol-file elf-file\n", name);
+    fprintf(stderr, " Convert a DOL file to an ELF file (by segments)\n");
+    fprintf(stderr, " Options:\n");
+    fprintf(stderr, "  -h    Show this help\n");
+    fprintf(stderr, "  -v    Be more verbose (twice for even more)\n");
+}
+
+int 
+main(int argc, char** argv) {
     char **arg;
+    int info;
+    const char *dol_file;
+    const char *elf_file;
+
+    ELF_map map;
 
     if(argc < 2) {
         usage(argv[0]);
@@ -460,39 +471,39 @@ int main(int argc, char** argv)
     arg = &argv[1];
     argc--;
 
-    while(argc && *arg[0] == '-') {
-        if(!strcmp(*arg, "-h")) {
-            usage(argv[0]);
-            return 1;
-        } else if(!strcmp(*arg, "-v")) {
-            verbosity++;
-        } else if(!strcmp(*arg, "--")) {
-            arg++;
-            argc--;
-            break;
-        } else {
-            fprintf(stderr, "Unrecognized option %s\n", *arg);
-            usage(argv[0]);
-            return 1;
+    while(argc && (*arg)[0] == '-') {
+        switch((*arg)[1]) {
+            case 'h': usage(argv[0]); return 1;
+            case 'v': ++verbosity; break;
+            case 'i': info=1; break;
+            case '-': ++arg; --argc; goto filenames;
+            default:
+                      fprintf(stderr, "Unrecognized option %s\n", *arg);
+                      usage(argv[0]);
+                      return 1;
         }
         arg++;
         argc--;
     }
-    if(argc < 2) {
-        usage(argv[0]);
-        exit(1);
+
+filenames:
+    if(!info && argc < 2) argc = 0;
+    elf_file=dol_file=0;
+    switch(argc) {
+        case 2: elf_file = arg[1];
+        case 1: dol_file = arg[0]; break;
+        default:
+                usage(argv[0]);
+                exit(1);
     }
-
-    const char *dol_file = arg[0];
-    const char *elf_file = arg[1];
-
-    ELF_map map;
 
     memset(&map, 0, sizeof(map));
 
-    read_dol_segments(&map, dol_file);
-    map_elf(&map);
-    write_elf(&map, elf_file);
-
+    if(info) print_dol_hdr(dol_file);
+    if(argc > 2) {
+        read_dol_segments(&map, dol_file);
+        map_elf(&map);
+        write_elf(&map, elf_file);
+    }
     return 0;
 }
